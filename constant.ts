0be280e1726e8ee1140591faca5ce99216138e13
Thar Lin Shwe <albent2017@gmail.com>
2024-08-12 15:29:31 +0800

    Implemented utilities function for hashing

--- constant.ts
+++ constant.ts
@@ -1,3 +1,8 @@
 export const PORT = 3000;
+
+// JWT
 export const JWT_SECRET = process.env.JWT_SECRET || "test";
 export const JWT_LIFETIME = process.env.JWT_LIFETIME || "2 days";
+
+// Hashing
+export const SALT_ROUNDS: number = process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10;
