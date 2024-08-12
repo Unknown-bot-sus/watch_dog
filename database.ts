1729cb6b769d1dab28354544f7461dde5af5d23e
Thar Lin Shwe <albent2017@gmail.com>
2024-08-12 16:16:49 +0800

    Added util funtions to user model

--- database.ts
+++ database.ts
@@ -1,3 +1,30 @@
 import { PrismaClient } from "@prisma/client";
+import { compareHash, hashMessage } from "./utils/hashMessage";
+import { sign } from "jsonwebtoken";
+import { JWT_LIFETIME, JWT_SECRET } from "./constant";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient().$extends({
+    query: {
+        user: {
+            async create({ args, query}) {
+                args.data.passwrod = await hashMessage(args.data.passwrod);
+                return query(args)
+            }
+        }
+    },
+    model: {
+        user: {
+            async comparePassword(password: string, hashedPassword: string) {
+                return await compareHash(password, hashedPassword);
+            },
+
+            async createJWT(id: number) {
+                return sign(
+                    {id},
+                    JWT_SECRET,
+                    {expiresIn: JWT_LIFETIME}
+                )
+            }
+        }
+    }
+});
