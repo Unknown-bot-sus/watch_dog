c4560314ad420721404e020c45ac4bb29d162d0c
Thar Lin Shwe <albent2017@gmail.com>
2024-08-29 19:13:39 +0800

    Increased request size limit

--- index.ts
+++ index.ts
@@ -22,8 +22,8 @@ const server = http.createServer(app);
 app.use(cors());
 
 app.use(express.static(path.join(process.cwd(), "public")));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+app.use(express.json({limit: "50mb"}));
 
 app.use("/", viewRouter);
 app.use("/api/v1/auth", authRouter);
@@ -36,6 +36,6 @@ app.use("/api/v1/detections", detectionsRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
 })  
